472f300522af5380f7dc5cde33c393bf780d6408
Mike Hole <mike@mikehole.com>
2018-01-04 10:19:06 +0000

    Test continuous deployment

--- ColourMatch/ExecuteMatch.cs
+++ ColourMatch/ExecuteMatch.cs
@@ -16,19 +16,19 @@ namespace ColourMatch
             log.Info("C# HTTP trigger function processed a request.");
 
             // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+            string imageUrl = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "url", true) == 0)
                 .Value;
 
             // Get request body
             dynamic data = await req.Content.ReadAsAsync<object>();
 
             // Set name to query string or body data
-            name = name ?? data?.name;
+            imageUrl = imageUrl ?? data?.url;
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return imageUrl == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass the url of the image to process on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "You requested to check:" + imageUrl);
         }
     }
 }
